765104a4f782d9f790f3e3807e1126646806272e
iznaur-hac <obito20077@gmail.com>
2025-07-19 19:15:05 +0300

    add config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+
+    return [
+        new webpack.ProgressPlugin(),
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
+    ]
+}
