56b71b5c9937ad212fc310f90f14d6175c3a9ad6
iznaur-hac <obito20077@gmail.com>
2025-07-28 21:35:37 +0300

    added AppLink and Navbar components

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,34 @@
+import { Link, LinkProps } from 'react-router-dom';
+import cls from './AppLink.module.scss'
+import { classNames } from "shared/lib/classNames/calssNames"
+import { FC } from 'react';
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps{
+    className?: string;
+    theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+    const {
+        to, 
+        children, 
+        className, 
+        theme = AppLinkTheme.PRIMARY, 
+        ...otherProps
+    } = props;
+
+    return (
+        <Link 
+            to={to} 
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    )
+}
