745523f9200cb9b950c25d8d6d1847a366ea7851
iznaur-hac <obito20077@gmail.com>
2025-07-29 01:22:43 +0300

    added images loaders and moved theme switched button into a separate component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,35 +4,51 @@ import { BuildOptions } from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+    };
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+
     const cssLoaders = {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-                                localIdentName: isDev 
-                                ? '[path][name]__[local]--[hash:base64:5]' 
-                                : '[hash:base64:8]'
-                            },
-                        }
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+                        localIdentName: isDev 
+                        ? '[path][name]__[local]--[hash:base64:5]' 
+                        : '[hash:base64:8]'
                     },
-                    "sass-loader",
-            ],
-        };
+                }
+            },
+            "sass-loader",
+        ],
+    };
 
     // если бы не использовали ts loader то понадобился бы специально babel loader для обработки jsx
     const typescriptLoader = {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            };
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        };
 
 
     return [
-            typescriptLoader,
-            cssLoaders
-        ];
+        fileLoader,
+        svgLoader,
+        typescriptLoader,
+        cssLoaders
+    ];
 }
